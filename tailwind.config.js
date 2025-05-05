75b165564b5dcec8c7bd8d4816765c5e251e688a
Excel Ikenna <ikennaexcel@gmail.com>
2025-05-05 20:15:31 +0100

    custom styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customBlue: "#2563EB",
+        customPurple: "#BF2EF0"
+        
+      }
+    },
   },
   plugins: [],
 }
