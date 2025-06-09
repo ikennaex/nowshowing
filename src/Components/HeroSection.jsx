5258e8ba673b0186d9b1412a1489efa5a92f4d80
Fortune <fortuneokechukwu206@gmail.com>
2025-06-09 14:53:54 +0100

    herosection, searchbar

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -1,20 +1,50 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/effect-cards';
 import { EffectCards, Autoplay } from 'swiper/modules';
 import movies from '../data/movies';
+import { baseUrl } from '../baseUrl';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const HeroSection = () => {
+  const [fetchedCinemaMovies, setFetchedCinemaMovies] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
   const [activeIndex, setActiveIndex] = useState(0);
-  const currentMovie = movies[activeIndex];
+
+  useEffect(() => {
+    const fetchCinemaMovies = async () => {
+      try {
+        const response = await axios.get(`${baseUrl}cinema`);
+        setFetchedCinemaMovies(response.data);
+        console.log(response.data);
+        setActiveIndex(0);
+      } catch (err) {
+        setError('Failed to fetch movies');
+        console.error(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchCinemaMovies();
+  }, []);
+
+  
+  const currentMovie = fetchedCinemaMovies[activeIndex];
+
+  if (loading || !fetchedCinemaMovies.length) {
+  return <div className="text-white text-center">Loading...</div>;
+}
 
   return (
 <div className="hero-section relative w-full h-[120vh] overflow-hidden mt-0">
   {/* Background Image */}
   <div
     className="hero-bg absolute inset-0 bg-cover bg-center bg-no-repeat brightness-10 z-0"
-    style={{ backgroundImage: `url(${currentMovie.image})` }}
+    style={{ backgroundImage: `url(${currentMovie.posterUrl})` }}
   ></div>
 
   {/* Gradient overlay to fade bottom */}
@@ -24,7 +54,7 @@ const HeroSection = () => {
   <div className="relative z-20 flex flex-col md:flex-row items-center justify-center h-full px-6 gap-4 md:gap-6">
     <div className="hero-overlay w-full md:w-1/2 text-white text-center md:text-left min-h-[70vh] md:min-h-full flex flex-col justify-center">
       <h1 className="text-3xl md:text-5xl font-bold mb-4">{currentMovie.title}</h1>
-      <p className="text-lg md:text-xl max-w-xl">{currentMovie.body}</p>
+      <p className="text-lg md:text-xl max-w-xl">{currentMovie.synopsis}</p>
 
       <button className="mt-6 w-fit bg-white text-black font-semibold py-2 px-6  rounded-xl hover:bg-black hover:text-white transition book-now">
         Movie details
@@ -33,21 +63,22 @@ const HeroSection = () => {
 
     <div className="w-80 h-50 md:w-80 md:h-50 md:mt-0 mb-12">
       <Swiper
-        effect={'cards'}
-        grabCursor={true}
+        effect='coverflow'
+        grabCursor={false}
         modules={[EffectCards, Autoplay]}
         autoplay={{
-          delay: 5000,
+          delay: 4000,
           disableOnInteraction: false,
         }}
         loop={true}
+        initialSlide={0}
         onSlideChange={(swiper) => setActiveIndex(swiper.realIndex)}
         className="mySwiper"
       >
-        {movies.map((movie, idx) => (
+        {fetchedCinemaMovies.map((movie, idx) => (
           <SwiperSlide key={idx}>
             <img
-              src={movie.image}
+              src={movie.posterUrl}
               alt={movie.title}
               className="w-full h-[60vh] md:h-[70vh] object-cover rounded-xl"
             />
